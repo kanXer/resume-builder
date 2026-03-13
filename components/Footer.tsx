571c05c73b7f78a0b94449020fa191818eef2ba3
Sahil Lala <user.kanxer@gmail.com>
2026-03-13 17:48:08 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,8 +25,8 @@ export default function Footer() {
               Next-gen resume builder, Gemini AI ki accuracy ke saath.
             </p>
             <div className="flex gap-5">
-              <a href="#" className="text-gray-400 hover:text-blue-600 transition"><Twitter size={20} /></a>
-              <a href="#" className="text-gray-400 hover:text-blue-600 transition"><Linkedin size={20} /></a>
+              <a href="https://x.com/itsSrivastava_" className="text-gray-400 hover:text-blue-600 transition"><Twitter size={20} /></a>
+              <a href="https://www.linkedin.com/in/kanxer/" className="text-gray-400 hover:text-blue-600 transition"><Linkedin size={20} /></a>
               <a href="https://github.com/kanXer" target="_blank" className="text-gray-400 hover:text-gray-900 transition"><Github size={20} /></a>
             </div>
           </div>
@@ -86,4 +86,5 @@ export default function Footer() {
       </div>
     </footer>
   );
+
 }
